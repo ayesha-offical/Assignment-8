dd490a692105f06d8679e4a4b2ca83dcddcc790c
ayesha-offical <ayeshamughal2513@gmail.com>
2025-01-06 23:44:07 +0500

    first commit

---
+++ sanity/schemaTypes/postType.ts
@@ -0,0 +1,51 @@
+import {DocumentTextIcon} from '@sanity/icons'
+import {defineArrayMember, defineField, defineType} from 'sanity'
+
+export const postType = defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  icon: DocumentTextIcon,
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+    }),
+    defineField({
+      name: 'author',
+      type: 'string',
+      
+    }),
+    defineField({
+      name: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+        }
+      ]
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'content',
+      title: 'description',
+      type: 'text',
+    }),
+  ],
+ 
+})
